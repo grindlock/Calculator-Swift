8d2d8bae8389e78eccbab512181eb2ca10f7f484
grindlock <sergioenriqueperez@gmail.com>
2017-01-13 01:08:00 -0500

    UI is finish and some minor bugs has been fix.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,37 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // Here I made an infere type constant
     let defaultTxt = "0.0"
+    // Here I made making an explecit type constant
+    let cRadius:CGFloat = 10
     
+    enum Oper:String{
+        case Division = "/"
+        case Multiplication = "*"
+        case Subtraction = "-"
+        case Addition = "+"
+        case Mod = "%"
+        case Negative = "*(-1)"
+        case Empty = "Empty"
+    }
+    
+    var grabTxt=""
+    var operSelected = Oper.Empty
+    
+  
+    
+    @IBOutlet weak var screenBackground: UIView!
     @IBOutlet weak var screen:UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        screen.layer.cornerRadius = cRadius
+        screen.layer.masksToBounds = true
+        screenBackground.layer.cornerRadius = cRadius
+        
         screen.text = defaultTxt
-        //var txt = add()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    /*@IBAction func add ( btn: UIButton)->Int{
-        
-        switch(btn.tag){
-        case 1: return 1
-        case 2: return 2
-        case 3: return 3
-            
-        default: return 0
-        }
-        return 0
-    }*/
+   
 
-    @IBAction func equalBtnPressed(_ sender: UIButton) {
+    @IBAction func numBtnPressed(sender: UIButton){
+        
+        if (screen.text?.characters.count)! < 12
+        {
+            grabTxt += "\(sender.tag)"
+            screen.text = grabTxt
+        }
+    
     }
+  
 
 }
 
